32dd42910e9b9923b8d52a5eb7f07c2931db7b77
RandyChen <jushanc2@illinois.edu>
2022-11-28 14:12:38 -0600

    problem encountered in centralized solver for 3 drones

--- distributed_mpc/problem.py
+++ distributed_mpc/problem.py
@@ -3,6 +3,7 @@ from casadi import *
 import do_mpc
 from dynamics import *
 from util import *
+import networkx as nx
 
 g = 9.81
 
@@ -13,6 +14,7 @@ class quadProblem:
     def __init__(self, dynamics):
         self.dynamics = dynamics #Mluti-dynamical model
         
+        self.generate_graph(graph_type=graph_type, params=graph_params)
         
     @property
     def ids(self):
@@ -51,6 +53,38 @@ class quadProblem:
         Ui = U[:, ext_ind * u_dim : (ext_ind + 1) * u_dim]
 
         return Xi, Ui
+    
+    
+    def generate_graph(self, graph_type='expander', params=None):
+        '''Generate connected connectivity graph according to the params.'''
+
+        if graph_type == 'expander':
+            G = nx.paley_graph(self.n_agent).to_undirected()
+        elif graph_type == 'grid':
+            G = nx.grid_2d_graph(*params)
+        elif graph_type == 'cycle':
+            G = nx.cycle_graph(self.n_agent)
+        elif graph_type == 'path':
+            G = nx.path_graph(self.n_agent)
+        elif graph_type == 'star':
+            G = nx.star_graph(self.n_agent - 1)
+        elif graph_type == 'er':
+            if params < 2 / (self.n_agent - 1):
+                log.fatal("Need higher probability to create a connected E-R graph!")
+            G = None
+            while G is None or nx.is_connected(G) is False:
+                G = nx.erdos_renyi_graph(self.n_agent, params)
+        else:
+            log.fatal('Graph type %s not supported' % graph_type)
+
+        self.n_edges = G.number_of_edges()
+        self.G = G
+
+    def plot_graph(self):
+        '''Plot the generated connectivity graph.'''
+
+        plt.figure()
+        nx.draw(self.G)
     
     def __repr__(self):
         return f"MPCProblem(\n\t{self.dynamics})"
