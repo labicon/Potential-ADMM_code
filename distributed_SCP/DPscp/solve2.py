b06591e5d970eaa0cb13ceddfec49fad82504d57
RandyChen <jushanc2@illinois.edu>
2023-03-19 09:54:09 -0500

    troubleshooting problem infeasibility

--- distributed_SCP/DPscp/solve2.py
+++ distributed_SCP/DPscp/solve2.py
@@ -9,6 +9,7 @@ from functools import partial
 import dpilqr as dec
 # from animations import animate_cartpole
 
+COLLISION_RADIUS = 0.35
 
 @partial(jax.jit, static_argnums=(0,))
 @partial(jax.vmap, in_axes=(None, 0, 0))
@@ -36,7 +37,7 @@ def solve_scp(fd: callable,
                       ρ: float,
                       tol: float,
                       max_iters: int):
-    """Solve the cart-pole swing-up problem via SCP. This function is used for one-shot optimization"""
+    """This function is used for one-shot optimization"""
     n = Q.shape[0]  # state dimension
     m = R.shape[0]  # control dimension
 
@@ -50,9 +51,14 @@ def solve_scp(fd: callable,
     # Do SCP until convergence or maximum number of iterations is reached
     converged = False
     obj_prev = np.inf
+    iterate = 0
+    s_prev = None
     for i in (prog_bar := tqdm(range(max_iters))):
         s, u, obj = scp_iteration(fd, P, Q, R, N, s_bar, u_bar, s_goal, s0,
-                                  ru, ρ)
+                                  ru, ρ, iterate, s_prev)
+        
+        iterate+=1
+
         diff_obj = np.abs(obj - obj_prev)
         prog_bar.set_postfix({'objective change': '{:.5f}'.format(diff_obj)})
 
@@ -64,9 +70,11 @@ def solve_scp(fd: callable,
             obj_prev = obj
             np.copyto(s_bar, s)
             np.copyto(u_bar, u)
+        
+        s_prev = s
 
     if not converged:
-        raise RuntimeError('SCP did not converge!')
+        raise RuntimeError('SCP di not converge!')
 
     return s, u
 
@@ -74,7 +82,7 @@ def solve_scp(fd: callable,
 def scp_iteration(fd: callable, P: np.ndarray, Q: np.ndarray, R: np.ndarray,
                   N: int, s_bar: np.ndarray, u_bar: np.ndarray,
                   s_goal: np.ndarray, s0: np.ndarray,
-                  ru: float, ρ: float):
+                  ru: float, ρ: float, iterate: int, s_prev: np.ndarray):
     """Solve a single SCP sub-problem for the cart-pole swing-up problem."""
     A, B, c = linearize(fd, s_bar[:-1], u_bar)
     A, B, c = np.array(A), np.array(B), np.array(c)
@@ -91,19 +99,33 @@ def scp_iteration(fd: callable, P: np.ndarray, Q: np.ndarray, R: np.ndarray,
     constraints +=[s_cvx[0,:] == s_bar[0]]
 
     for k in range(N):
+        
+        objective += cvx.quad_form(s_cvx[k,:] - s_goal, Q) + cvx.quad_form(u_cvx[k,:], R) 
+        constraints += [s_cvx[k+1,:] == A[k]@s_cvx[k,:] + B[k]@u_cvx[k,:] + c[k]]
 
-      objective += cvx.quad_form(s_cvx[k,:] - s_goal, Q) + cvx.quad_form(u_cvx[k,:], R) 
-      constraints += [s_cvx[k+1,:] == A[k]@s_cvx[k,:] + B[k]@u_cvx[k,:] + c[k]]
-
-      for id in range(n_drones):
-
-        constraints += [-np.array([-np.pi/6, -np.pi/6, 0]) <= u_cvx[k,id*3:(id+1)*3], \
-                        u_cvx[k,id*3:(id+1)*3] <= np.array([np.pi/6, np.pi/6, 20])]
+        #Note: without the following convex trust regions, the solution blows up 
+        #just after a few iterations    
     
-      #Note: without the following convex trust regions, the solution blows up 
-      #just after a few iterations
-        # constraints += [cvx.pnorm(s_cvx[k,id*3:(id+1)*3]-s_bar[k,id*3:(id+1)*3],'inf') <= ρ]
-        # constraints += [cvx.pnorm(u_cvx[k,id*3:(id+1)*3]-u_bar[k,id*3:(id+1)*3],'inf') <= ρ] 
+        for i in range(n_drones):
+            constraints += [cvx.pnorm(s_cvx[k,i*3:(i+1)*3]-s_bar[k,i*3:(i+1)*3],'inf') <= ρ]
+            constraints += [cvx.pnorm(u_cvx[k,i*3:(i+1)*3]-u_bar[k,i*3:(i+1)*3],'inf') <= ρ] 
+            
+            #actuator constraints
+            constraints += [-np.array([-np.pi/6, -np.pi/6, 0]) <= u_cvx[k, i*3:(i+1)*3], \
+                        u_cvx[k, i*3:(i+1)*3] <= np.array([np.pi/6, np.pi/6, 20])]
+        
+            # linearized collision avoidance constraints
+            if iterate > 0:
+                prev_pos = [s_prev[k][id:id+6] for id in range(0, len(s_prev[k]), 6)]
+                curr_pos = [s_cvx[k][id:id+6] for id in range(0, len(s_cvx[k]), 6)]
+                for j in range(n_drones):
+                    if j != i:
+                        constraints+= [cvx.norm(prev_pos[i][0:3]-prev_pos[j][0:3]) + \
+                                    (prev_pos[i][0:3].T-prev_pos[j][0:3].T)/cvx.norm(prev_pos[i][0:3]-prev_pos[j][0:3]) \
+                                    * (curr_pos[i][0:3]-curr_pos[j][0:3]) >= COLLISION_RADIUS]
+
+    
+      
 
     objective += cvx.quad_form(s_cvx[-1,:] - s_goal, P)
     
@@ -146,7 +168,7 @@ def multi_Quad_Dynamics(s, u):
      # Define the constants of the quadrotor dynamics
     g = 9.81
     
-    # Split the state `s` and control input `u` into individual components
+    # Split the state `s` and control input `u` into indiviual components
     xs = [s[i:i+6] for i in range(0, len(s), 6)]
     us = [u[i:i+3] for i in range(0, len(u), 3)]
     
@@ -182,16 +204,18 @@ def discretize(f, dt):
     return integrator
 
 
-"""Run SCP in centralized receding-horizon:"""
+"""Main loop"""
 
 n_agents = 2
 n_states = 6
 n_controls = 3
 g = 9.81
-n = 12                                # state dimension
-m = 6                                # control dimension
+n = n_agents * n_states                                # state dimension
+m = n_agents * n_controls                              # control dimension
+
 s_goal = np.array([-1.5, -1.5, 1.2, 0,  0 , 0, 2.5, 1.5, 1.5, 0, 0 , 0])  # desired state
-s0 = np.array([0, 0, 0.8, 0, 0, 0, 1.5, 1.0, 1.0, 0, 0, 0])          # initial state
+s0 = np.array([0, 0, 1.0, 0, 0, 0, 1.5, 1.0, 1.0, 0, 0, 0])          # initial state
+
 dt = 0.1                             # discrete time resolution
 # T = 15.                              # total simulation time
 
@@ -204,7 +228,7 @@ fd = jax.jit(discretize(multi_Quad_Dynamics,dt))
 P = 1e3*np.eye(n)                    # terminal state cost matrix
 Q = np.eye(n)*10  # state cost matrix
 R = 1e-3*np.eye(m)                   # control cost matrix
-ρ = 3.                               # trust region parameter
+ρ = 1.5                               # trust region parameter
 ru = 8.                              # control effort bound
 tol = 5e-1                           # convergence tolerance
 max_iters = 100                      # maximum number of SCP iterations
@@ -212,55 +236,54 @@ max_iters = 100                      # maximum number of SCP iterations
 # Solve the swing-up problem with SCP
 # t = np.arange(0., T + dt, dt)
 # N = t.size - 1
-N = 15
-converged = False
+N = 50  
 
-count = 0
-si = s0
-obj_list = []
-
-# scp_iteration(fd: callable, P: np.ndarray, Q: np.ndarray, R: np.ndarray,
-#                   N: int, s_bar: np.ndarray, u_bar: np.ndarray,
-#                   s_goal: np.ndarray, s0: np.ndarray,
-#                   ru: float, ρ: float):
-
-
-u_bar = np.zeros((N, m))
-s_bar = np.zeros((N + 1, n))
-s_bar[0] = s0
-for k in range(N):
-    s_bar[k+1] = fd(s_bar[k], u_bar[k])
-
-
-X_trj =  np.zeros((0, n))  #Full trajectory over entire problem horizon (not just a single prediction horizon)
-STEP_SIZE=1
-"""TODO: the problem is blowing up: objective value grows unbounded somehow"""
-while not np.all(dec.distance_to_goal(si, s_goal.reshape(1,-1), n_agents = 2,n_states = 6,n_d= 3) < 0.1) :
-    count +=1
-    s, u, obj = scp_iteration(fd, P, Q, R, N, s_bar, u_bar, s_goal, si, ru, ρ)
-    obj_list.append(obj)
-    
-    print(f'current objective value is {obj}!')
-
-    si = s[STEP_SIZE,:]
-    X_trj = np.r_[X_trj, s[:STEP_SIZE]]
-
-    u_bar = np.zeros((N, m))
-    s_bar = np.zeros((N + 1, n))
-    s_bar[0] = si
-    for k in range(N):
-        s_bar[k+1] = fd(s_bar[k], u_bar[k])
-
-    if count > 50:
-        print('max. number of outer iterations reached!')
-        break
+s, u = solve_scp(fd, P, Q, R, N, s_goal, s0, ru, ρ, tol, max_iters)
 
 # Simulate open-loop control
 for k in range(N):
     s[k+1] = fd(s[k], u[k])
 
+
+
+
+# def solve_rhc():
+#     count = 0
+#     si = s0
+#     obj_list = []
+
+#     u_bar = np.zeros((N, m))
+#     s_bar = np.zeros((N + 1, n))
+#     s_bar[0] = s0
+#     for k in range(N):
+#         s_bar[k+1] = fd(s_bar[k], u_bar[k])
+
+#     X_trj =  np.zeros((0, n))  #Full trajectory over entire problem horizon (not just a single prediction horizon)
+#     STEP_SIZE=1
+#     """TODO: the problem is blowing up: objective value grows unbounded somehow"""
+#     while not np.all(dec.distance_to_goal(si, s_goal.reshape(1,-1), n_agents = 2,n_states = 6,n_d= 3) < 0.1) :
+#         s, u, obj = scp_iteration(fd, P, Q, R, N, s_bar, u_bar, s_goal, si, ru, ρ)
+#         count +=1
+        
+#         obj_list.append(obj)
+        
+#         print(f'current objective value is {obj}!')
+
+#         si = s[STEP_SIZE,:]
+#         X_trj = np.r_[X_trj, s[:STEP_SIZE]]
+
+#         u_bar = np.zeros((N, m))
+#         s_bar = np.zeros((N + 1, n))
+#         s_bar[0] = si
+#         for k in range(N):
+#             s_bar[k+1] = fd(s_bar[k], u_bar[k])
+
+#         if count > 50:
+#             print('max. number of outer iterations reached!')
+#             break
+
 # Plot state and control trajectories
-print(f'Full trajectory has shape {X_trj.shape}')
-fig = plt.figure(dpi=200)
-dec.plot_solve(X_trj,0,s_goal,x_dims,n_d = 3)
-plt.savefig('2_quad_SCP.png')
+# print(f'Full trajectory has shape {X_trj.shape}')
+# fig = plt.figure(dpi=200)
+# dec.plot_solve(X_trj,0,s_goal,x_dims,n_d = 3)
+# plt.savefig('2_quad_SCP.png')
