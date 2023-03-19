79b9f54a688ba3dac8cd2aaa8056974e33879176
Arna-Bhardwaj <90980107+Arna-Bhardwaj@users.noreply.github.com>
2023-03-18 22:15:06 -0400

    added more to the scp centralized code

--- distributed_SCP/centralized_scp_python_code.py
+++ distributed_SCP/centralized_scp_python_code.py
@@ -1,6 +1,49 @@
 #code for centralized version of scp 
-#init all solutions: 
 import numpy as np
+import cvxpy as cvx
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from functools import partial
+import dpilqr as dec
+# from animations import animate_cartpole
+
+
+@partial(jax.jit, static_argnums=(0,))
+@partial(jax.vmap, in_axes=(None, 0, 0))
+
+
+
+T_duration= 15
+dt= 0.01
+
+range_possible_pos= np.array([[-3, 3], [-3, 3], [-3, 3]])
+drone_distance= 1 #meter
+max_acceleration= 3.
+num_drones=4
+#initial positions for 4 drones 
+pi1= np.array([[1, 1, 1]])
+pi2= np.array([[1.5, 1.5, 1.5]])
+pi3= np.array([[-1, -1, -1]])
+pi4=np.array([[-1.5, -1.5, -1.5]])
+   
+#final positions
+
+    pf1=np.array([[-1, -1, -1]])
+    pf2=np.array([[-1.5, -1.5, -1.5]])
+    pf3=np.array([[1, 1., 1]])
+    pf4=np.array([[1.5, 1.5, 1.5]])
+
+def linearize(fd:callable, s: jnp.ndarray, u:jnp.ndarray):
+    
+    A = jax.jacfwd(fd,0)(s,u)
+    B = jax.jacfwd(fd,1)(s,u)
+    c = fd(s,u)-A@s-B@u
+    
+    
+    return A, B, c
+
 
 def initAllSolutions(po, pf, h, K):
     po = np.squeeze(po)
@@ -13,3 +56,19 @@ def initAllSolutions(po, pf, h, K):
         for k in range(len(t)):
             p[:,k,i] = po[:,i] + t[k]*diff/((K-1)*h)
     return p
+
+
+
+
+
+    #i need to convert velocity to acceleration now 
+def collision_constraints(A, b):
+   #define collision constraints  
+
+
+def centralizedSCP (po,pf,h,K,N,pmin,pmax,rmin,alim,A_p, A_v,E1,E2,order):
+    prev_pos= initAllSolutions(po, pf, h, K)
+
+    upper_bound= max_acceleration
+
+    #you need the solve QP part and inequality constraints 
